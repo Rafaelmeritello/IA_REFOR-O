4e4f8b2305d4531fc52b0e956e1496b6826e08c9
Rafaelmeritello <rafaelmeritello@gmail.com>
2023-11-15 21:12:13 -0300

    sistema grafico incluido

--- main.py
+++ main.py
@@ -7,13 +7,20 @@ import os
 import random
 from time import sleep
 import uteis
+import pygame
 
-input()
+
+running = True
+BRANCO = (255, 255, 255)
+AZUL = (0, 0, 255)
+AMARELO = (255, 255, 0)
+VERDE = (0, 255, 0)
+
 try:
     import matplotlib.pyplot as plt
 except ImportError as err:
     print("Erro de importação:", err)
-input()
+
 
 exploration_rates = []
 memory_sizes = []
@@ -21,12 +28,12 @@ memory_sizes = []
 rec_total = 0
 desc_temporal = 1
 
-exploracao = 0.99
+exploracao = 0.98
 FATOR_EXPLORACAO_DECAIMENTO = 0.9991
 exploracao_minima = 0.20
 
 DISTANCIA_PERTO = 1
-MAX_ITENS = 7910
+MAX_ITENS = 8110
 TEMPO_ESPERA = 0
 
 
@@ -34,7 +41,7 @@ TEMPO_ESPERA = 0
 def cls():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-
+#add objetos
 def add_objetos(ambiente, pos_agente=5):
     pos_bandeira = random.randint(0, len(ambiente.getcenario()) - 1)
     pos_objetivo = random.randint(0, len(ambiente.getcenario()) - 1)
@@ -66,11 +73,56 @@ estado = {
 }
 
 
-def testar_agente(agente, ambiente, estado):
+
+
+#teste do agente
+TAMANHO_BLOCO = 65
+LARGURA_TELA = 800  # Ajuste conforme necessário
+
+# Criar a janela Pygame uma vez antes do loop principal
+LARGURA_TELA = 800  # Ajuste conforme necessário
+
+
+def desenhar_cenario(ambiente, screen, conclusao=False):
+    # Calcular a posição inicial para centralizar os blocos na tela
+    posicao_inicial_x = (LARGURA_TELA - len(ambiente.getcenario()[0]) * TAMANHO_BLOCO) // 2
+    posicao_x = posicao_inicial_x  # Definir a posição horizontal inicial
+
+    for y, linha in enumerate(ambiente.getcenario()):
+        for celula in linha:
+            cor = BRANCO  # Cor padrão para células vazias
+            if celula == "A":
+                cor = AZUL  # Agente em azul
+            elif celula == "B":
+                cor = AMARELO  # Bandeira em amarelo
+            elif celula == "O":
+                cor = VERDE  # Objetivo em verde
+
+            if conclusao:
+                cor = VERDE  # Tornar todos os blocos verdes em caso de conclusão
+
+            pygame.draw.rect(screen, cor, (posicao_x - 320, screen.get_height() / 2, TAMANHO_BLOCO, TAMANHO_BLOCO))
+
+            # Incrementar a posição horizontal para o próximo bloco
+            posicao_x += TAMANHO_BLOCO
+
+    pygame.display.flip()
+
+def testar_agente(agente, ambiente, estado, screen):
+    pygame.init()
+    exploracao = 0.14
+    conclusao_timer = 0  # Contador para rastrear o tempo desde a conclusão
+    conclusao_duration = 3  # Duração em segundos para mostrar todos os blocos verdes após a conclusão
+
     while True:
-        cls()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
         est_copy = estado.copy()
         acao = agente.escolher_acao(est_copy, exploracao)
+        sleep(0.22)
         agente.realizar_acao(acao)
 
         estado = {
@@ -79,33 +131,26 @@ def testar_agente(agente, ambiente, estado):
             "concluido": ambiente.concluido,
             "visao": ambiente.get_visao(DISTANCIA_PERTO, agente.get_posicao()),
             "bandeira_coletada": agente.bandeira_coletada,
-
         }
 
-        # Exibir o estado do ambiente e o cenário
-        print("Cenário:", ambiente.getcenario())
-        print("Ação:", acao)
-        print("Concluído:", estado["concluido"])
-        print("Distância da bandeira:", estado["distancia_bandeira"])
-        print("Distância do objetivo:", estado["distancia_objetivo"])
-        print("Bandeira coletada:", estado["bandeira_coletada"])
-        print("Visão:", estado["visao"])
-        print("taxa_exploracao " + str(exploracao))
-        print("-" * 20)
+        # Desenhar o cenário no Pygame
+        desenhar_cenario(ambiente, screen, conclusao=estado["concluido"])
 
-        # Use 0.8 segundos para o intervalo entre as ações
         if estado["concluido"]:
             rec_total = 0
             desc_temporal = 1
 
             ambiente.resetar()
             add_objetos(ambiente, agente.get_posicao())
-            print("concluiu")
-            sleep(4)
+            sleep(0.75)
+            conclusao_timer = pygame.time.get_ticks() / 1000  # Obter o tempo atual em segundos
 
-        sleep(0.8)
+        if conclusao_timer > 0 and pygame.time.get_ticks() / 1000 - conclusao_timer > conclusao_duration:
+            # Resetar o timer após a duração especificada
+            conclusao_timer = 0
 
-
+        sleep(0.1)
+#treinamento
 for i in range(MAX_ITENS):
 
     est_copy = estado.copy()
@@ -153,4 +198,6 @@ plt.legend()
 plt.grid()
 plt.title('Evolução da Taxa de Exploração e Taxa aprendizado')
 plt.show()
-testar_agente(agente, ambiente, estado)
+screen = pygame.display.set_mode((LARGURA_TELA, 720))
+testar_agente(agente, ambiente, estado,screen)
+
